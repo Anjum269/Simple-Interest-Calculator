aee4fd2475e7b5cab0bc5af2a7da1920e39d528d
Anjum <anjumkousar079@gmail.com>
2025-10-04 09:34:13 +0530

    Added Compound Interest calculation

--- interest_calculator.py
+++ interest_calculator.py
@@ -1,11 +1,15 @@
+ 
 # interest_calculator.py
 
-# Formula: SI = (P * R * T) / 100
+# SI = (P * R * T) / 100
+# CI = P * ( (1 + R/100)^T - 1 )
 
 P = float(input("Enter Principal Amount: "))
 R = float(input("Enter Rate of Interest: "))
 T = float(input("Enter Time (in years): "))
 
 SI = (P * R * T) / 100
+CI = P * ((1 + R / 100) ** T - 1)
 
 print(f"Simple Interest: ₹{SI}")
+print(f"Compound Interest: ₹{CI:.2f}")
